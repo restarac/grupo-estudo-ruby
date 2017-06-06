6425dfad6356e30938462cb6b4812845776ed6d7
Frederico Pereira <frederico.pereira@getninjas.com.br>
2017-06-06 15:07:35 -0300

    Finalizando grupo de estudos

--- git_tnterface_tests.rb
+++ git_tnterface_tests.rb
@@ -5,7 +5,7 @@ require "./git_interface"
 RSpec.describe GitInterface , "#add" do
   subject {GitInterface.new}
   it "add all" do
-    expect(subject.add).to include "nothing to commit"
+    expect(subject.status).to include "nothing to commit"
   end
 end
 
